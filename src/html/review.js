da70f4041749e226d8c6f6a472827ae5ce310174
Marco Pegoraro <marco.pegoraro@gmail.com>
2016-09-28 16:46:41 +0200

    #4 Merge cards

--- src/html/review.js
+++ src/html/review.js
@@ -1,8 +1,16 @@
 
-loadCards()
-    .then(cards => populateCards(cards, 'pros'))
-    .then(cards => populateCards(cards, 'cons'))
-    .catch(e => console.error(e));
+/**
+ * Fetch Cards and initialise the board
+ */
+
+
+fetchCards();
+$('#mergeCardsBtn').on('click', mergeCards);
+
+
+/**
+ * Init Sortable Behaviour
+ */
 
 $('#pros, #cons').sortable({
     connectWith: '.x-cards',
@@ -11,6 +19,15 @@ $('#pros, #cons').sortable({
 $('#pros').on('sortupdate', updateCardsPosition('pros'));
 $('#cons').on('sortupdate', updateCardsPosition('cons'));
 
+
+function fetchCards() {
+    $('#pros, #cons').empty();
+    $('#mergeCardsBtn').fadeOut();
+    loadCards()
+        .then(cards => populateCards(cards, 'pros'))
+        .then(cards => populateCards(cards, 'cons'))
+        .catch(e => console.error(e));
+}
 
 function loadCards() {
     return new Promise((resolve, reject) => {
@@ -59,6 +76,26 @@ function updateCardsPosition(type) {
     };
 }
 
+function mergeCards(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $(this).blur();
+
+    var data = {
+        cards: selectedCards,
+    };
+
+    $.ajax({
+        method: 'put',
+        url: '/cards/merge',
+        contentType: 'application/json',
+        data: JSON.stringify(data),
+        dataType: 'json',
+        success: () => fetchCards(),
+        error: err => console.error(err),
+    });
+}
+
 function populateCards(cards, type) {
     var $target = $('#' + type);
 
@@ -90,9 +127,35 @@ function makeCard(card) {
         .catch(err => console.error(err));
     });
 
+    $card.on('click', toggleCardSelection)
+
     return $card;
 }
 
 function fill($target, field, value) {
-    $target.find('[data-field=' + field + ']').text(value);
+    $target.find('[data-field=' + field + ']').html(value.replace(/\n/g, "<br />"));
+}
+
+var selectedCards = [];
+
+function toggleCardSelection(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    var $card = $(this);
+    var cardId = $card.attr('data-card-id');
+
+    if (selectedCards.indexOf(cardId) === -1) {
+        selectedCards.push(cardId);
+        $card.addClass('active');
+    } else {
+        selectedCards.splice(selectedCards.indexOf(cardId), 1);
+        $card.removeClass('active');
+    }
+
+    if (selectedCards.length) {
+        $('#mergeCardsBtn').fadeIn();
+    } else {
+        $('#mergeCardsBtn').fadeOut();
+    }
 }
